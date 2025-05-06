003fca93b7a1ab26e250bb93cd25999e61c9cd52
suzatachaudhary <suzatac3@gmail.com>
2025-05-06 13:46:42 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,45 @@
+class SwitchCase
+{
+  public static void main(String[] args) 
+  {
+    int day=3;
+    switch(day){
+      case 1:
+        {
+          System.out.println("Monday");
+          break;
+        }
+      case 2:
+        {
+          System.out.println("tuesday");
+          break;
+        }
+      case 3:
+        {
+          System.out.println("Wednesday");
+          break;
+        }
+      case 4:
+        {
+          System.out.println("thursday");
+          break;
+        }
+      case 5:
+        {
+          System.out.println("friday");
+          break;
+        }
+      case 6:
+        {
+          System.out.println("Saturday");
+          break;
+        }
+      case 7:
+        {
+          System.out.println("Sunday");
+          break;
+        }
+    }
+
+ }
+}
